b967d5dc05b9518902a1686989244ebf12949050
Aman Jain <83697226+amanjain5132@users.noreply.github.com>
2022-12-29 15:13:11 +0530

    Create Input currency in rupees and output in USD.java

---
+++ DSA ASSIGNMENTS/02-first-java/Input currency in rupees and output in USD.java
@@ -0,0 +1,4 @@
+ System.out.println("enter the INR Amount you want to Convert into USD $$ : ");
+        float C = sc.nextFloat();
+        float result = C * 82.83f;
+        System.out.println("The INR Amount To usd Is : "+result);
