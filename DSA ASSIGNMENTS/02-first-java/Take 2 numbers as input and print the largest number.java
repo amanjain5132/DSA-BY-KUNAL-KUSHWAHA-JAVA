1589f6df3a4cea3cb96adb812eba5002fd5c88c1
Aman Jain <83697226+amanjain5132@users.noreply.github.com>
2022-12-29 15:11:45 +0530

    Create Take 2 numbers as input and print the largest number.java

---
+++ DSA ASSIGNMENTS/02-first-java/Take 2 numbers as input and print the largest number.java
@@ -0,0 +1,13 @@
+System.out.println("Enter the First number : ");
+        System.out.println("Enter the Second number : ");
+        int F1 = sc.nextInt();
+        int F2 = sc.nextInt();
+
+        if( F1 > F2)
+        {
+            System.out.println("the largest Number Between 2 is : "+F1);
+        }
+        else
+        {
+            System.out.println("the largest Number Between 2 is : "+F2);
+        }
