d0f1c274e6ec0c3bdd73ce057e459ef69f6e66f3
Aman Jain <83697226+amanjain5132@users.noreply.github.com>
2022-12-29 15:07:06 +0530

    Add files via upload

---
+++ DSA ASSIGNMENTS/02-first-java/ODD_EVEN.java
@@ -0,0 +1,18 @@
+package DSAKK;
+
+import java.util.Scanner;
+
+public class ODD_EVEN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number you want to check weather its odd or even");
+        int check = sc.nextInt();
+        if(check % 2 == 0){
+            System.out.println("even number : "  +check);
+        }
+        else
+        {
+            System.out.println("odd number");
+        }
+    }
+}
