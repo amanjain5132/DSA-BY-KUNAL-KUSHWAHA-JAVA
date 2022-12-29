f189668231d9c726b98a9ad056acaac6ea7bed86
Aman Jain <83697226+amanjain5132@users.noreply.github.com>
2022-12-29 15:09:55 +0530

    Create Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.java

---
+++ DSA ASSIGNMENTS/02-first-java/Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.java
@@ -0,0 +1,9 @@
+System.out.println("Enter the Principal Amount  : ");
+        System.out.println("Enter the Time  : ");
+        System.out.println("Enter the Rate  : ");
+        int P = sc.nextInt();
+        int T = sc.nextInt();
+        float R = sc.nextFloat();
+        Float SI = (P * T * R) / 100;
+        System.out.println(SI);
+
